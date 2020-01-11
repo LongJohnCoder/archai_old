e65b4ac853f19ea468cc0ac803e6a3982ff67b21
Shital Shah <shitals@microsoft.com>
2020-01-11 05:23:11 -0800

    refactor e2e script

--- scripts/random/cifar_e2e.py
+++ scripts/random/cifar_e2e.py
@@ -7,41 +7,72 @@ import shutil
 from FastAutoAugment.common.common import common_init, get_config_common
 
 if __name__ == '__main__':
+    # accept search and eval scripts to run
+    # config file can be supplied using --config
     parser = argparse.ArgumentParser(description='NAS E2E Runs')
-    parser.add_argument('--scripts', type=str,
-                        default='scripts/random/cifar_search.py,scripts/random/cifar_eval.py',
-                        help='scripts to run in order, comma separated')
+    parser.add_argument('--search-script', type=str,
+                        default='scripts/random/cifar_search.py',
+                        help='Search script to run')
+    parser.add_argument('--eval-script', type=str,
+                        default='scripts/random/cifar_eval.py',
+                        help='Eval script to run')
     parser.add_argument('--exp_prefix', type=str, default='random',
                         help='Experiment prefix to use')
     args, extra_args = parser.parse_known_args()
 
+    # load config to some of the settings like logdir
     conf = common_init(use_args=True)
+    logdir = get_config_common()['logdir']
+    assert logdir
 
-    last_output_file = None
-    for script in args.scripts.split(','):
-        script = os.path.expandvars((os.path.expanduser(script.strip())))
-        experiment_name = args.exp_prefix + '_' + Path(script).stem
+    # get script, resume flag and experiment dir for search
+    search_script = args.search_script
+    resume = conf['nas']['search']['resume']
+    search_script = os.path.expandvars((os.path.expanduser(search_script.strip())))
+    search_script =  os.path.abspath(search_script)
+    experiment_name = args.exp_prefix + '_' + Path(search_script).stem
+    experiment_dir = os.path.join(logdir, experiment_name)
 
-        # copy output of last experiment to this experiment's folder
-        logdir = get_config_common()['logdir']
-        assert logdir
-        experiment_dir = os.path.join(logdir, experiment_name)
-        os.makedirs(experiment_dir, exist_ok=True)
-        if last_output_file:
-            shutil.copy2(last_output_file, experiment_dir)
-
-        script =  os.path.abspath(script)
-        print(f'Starting {script}...')
+    # see if search has already produced the output
+    final_desc_filepath = os.path.join(experiment_dir, 'final_model_desc.yaml')
+    if not resume or not os.path.exists(final_desc_filepath):
+        print(f'Starting {search_script}...')
         result = subprocess.run(
-            ['python', script,
-             '--config', conf.config_filepath,
-             '--config-defaults', conf.config_defaults_filepath,
-             '--common.experiment_name', experiment_name
+            ['python', search_script,
+            '--config', conf.config_filepath,
+            '--config-defaults', conf.config_defaults_filepath,
+            '--common.experiment_name', experiment_name
             ])
-        print(f'Script {script} returned {result.returncode}')
-
+        print(f'Script {search_script} returned {result.returncode}')
         if result.returncode != 0:
-            exit(1)
+            exit(result.returncode)
 
-        # TODO: need to remove file name hard coding
-        last_output_file = os.path.join(experiment_dir, 'final_model_desc.yaml')
+    # get script, resume flag and experiment dir for eval
+    eval_script = args.eval_script
+    resume = conf['nas']['eval']['resume']
+    eval_script = os.path.expandvars((os.path.expanduser(eval_script.strip())))
+    eval_script =  os.path.abspath(eval_script)
+    experiment_name = args.exp_prefix + '_' + Path(eval_script).stem
+    experiment_dir = os.path.join(logdir, experiment_name)
+
+    # if eval has already produced the output, skip eval run
+    model_filepath = os.path.join(experiment_dir, 'model.pt')
+    if not resume or not os.path.exists(model_filepath):
+        # copy output of search to eval folder
+        os.makedirs(experiment_dir, exist_ok=True)
+        shutil.copy2(final_desc_filepath, experiment_dir)
+
+        print(f'Starting {eval_script}...')
+        result = subprocess.run(
+            ['python', eval_script,
+            '--config', conf.config_filepath,
+            '--config-defaults', conf.config_defaults_filepath,
+            '--common.experiment_name', experiment_name
+            ])
+        print(f'Script {eval_script} returned {result.returncode}')
+        if result.returncode != 0:
+            exit(result.returncode)
+
+    print('Search and eval done.')
+    exit(0)
+
